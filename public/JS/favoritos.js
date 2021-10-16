bca77ade3bc135032cbf1d91a7be40235be49bf5
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-16 20:07:50 +0200

    Secrea el post para generar la pag de favoritos - Solo local

---
+++ public/JS/favoritos.js
@@ -0,0 +1,9 @@
+import FavoritosPage from "./FavoritosPage.js";
+
+debugger;
+const pruebaPokemon = new FavoritosPage(
+  "body",
+  "componente",
+  "section",
+  "http://localhost:3001/posts"
+);
--- public/JS/index.js
+++ public/JS/index.js
@@ -1,6 +1,5 @@
 import Page from "./Page.js";
-
-debugger;
+import Service from "./Service.js";
 
 const pruebaPokemon = new Page(
   "body",
@@ -8,3 +7,16 @@ const pruebaPokemon = new Page(
   "section",
   "https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0"
 );
+
+/*let prueba = {
+  name: "minun",
+  url: "https://pokeapi.co/api/v2/pokemon/312/",
+};
+
+let urlPrueba = "http://localhost:3001/posts";
+
+let pruebaCreate = new Service(urlPrueba);
+//pruebaCreate.getService(urlPrueba);
+pruebaCreate.createElement(prueba, urlPrueba);
+
+//createElement(prueba, urlPrueba);*/
